bba5160b0099ff23da516cc15f03597bda8ed492
collins <collinsonyeka03@gmail.com>
2026-06-21 23:30:59 +0100

    second commit

--- select.js
+++ select.js
@@ -2,6 +2,8 @@ const image = document.querySelector('#img');
 const input = document.querySelector('#file');
 const text = document.querySelector('#txt');
 const count = document.querySelector('#count');
+const form = document.querySelector('#selector-form');
+const submitMessage = document.querySelector('#submit-message');
 
 input.addEventListener('change', () => {
   if (input.files && input.files[0]) {
@@ -17,3 +19,13 @@ text.oninput = () => {
 };
 
 text.oninput();
+
+if (new URLSearchParams(window.location.search).get('submitted') === 'true') {
+  submitMessage.innerText = 'Submitted';
+  submitMessage.classList.add('show');
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  window.location.href = 'select.html?submitted=true';
+});
